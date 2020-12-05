a3b218ab402a5a1b7fbdb47d7fd12beb5f571deb
Olga <zespolowe.fais@gmail.com>
2020-12-05 15:11:43 +0100

    help and new outputs for free and mount commands

--- main.py
+++ main.py
@@ -15,8 +15,32 @@ PROMPTS = ['root> ', 'user> ']
 def parse_arguments():
     parser = argparse.ArgumentParser()
     parser.add_argument("-f", "--full_mode", action="store_true",
-                        help="Use full mode if you use script on target host, which should be protected. " 
-                             "Without that, the script will be working correctly, but some system commands would not be blocked")
+                        help="Tryb full_mode sluży do uruchomienia skryptu na dockerze. "
+                             "W przeciwnym wypadku, jeśli chce sie go uruchomić na innym systemie, to należy uruchamiać skrypt bez tej opcji." 
+                             "Komendy oszukiwane przez honeypota:"
+                             "- wget - plik zostanie pobrany, ale do innego katalogu niż wskazany - do wget_downloads. "
+                             "Użytkownikowi natomiast zostanie wyświetlona fałszywa informacja o nieznalezieniu pliku pod danym linkiem"
+                             "- apt-get - nie można znaleźć wymienionej paczki"
+                             "- curl - nieznalezienie komendy"
+                             "- df - fałszywy output"
+                             "- free - fałszywy output"
+                             "- ifconfig - fałszywy output"
+                             "- init - brak outputu"
+                             "- killall - brak outputu"
+                             "- mc - nie znaleziono"
+                             "- pico - nie znaleziono"
+                             "- nano - nie znaleziono"
+                             "- vi/vim - nie znaleziono"
+                             "- mount - fałszywy output"
+                             "- reboot - brak outputu"
+                             "- shutdown - brak outputu"
+                             "- top - fałszywa informacja o braku wolnego miejsca na dysku"
+                             "- bash - pusty output, żeby myślano że komendę udało się wywołać"
+                             "- uname - w przypadku korzystania z dockera zostanie zwrócona komenda uname bez nazwy i wersji systemu"
+                             "Inne obsługiwane komendy:"
+                             "- cd - skrypt zapamiętuje każde przejście do innego katalogu i wtedy komendy wywołuje z niego"
+                             ""
+                             "")
     return parser.parse_args()
 
 
